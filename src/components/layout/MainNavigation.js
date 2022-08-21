e8e6bbf1e318edde03482fd06fb964325b2fd8f7
Chitrang <chitrang.patel@ncoregames.com>
2022-08-21 15:38:07 +0530

    49-display meetup items on Favourite section

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
 
+import { useContext } from "react";
+import FavouriteContext from "../../store/favourites-context";
+
 function MainNavigation() {
+  const favouritCtx = useContext(FavouriteContext);
+
   return (
     <div className={classes.header}>
       <h1 className={classes.logo}>Meetups</h1>
@@ -10,10 +15,13 @@ function MainNavigation() {
           <Link to="/">All Meetups</Link>
         </li>
         <li>
-          <Link to="/favourites">Favourites</Link>
+          <Link to="/new-meetups">New Meetups</Link>
         </li>
         <li>
-          <Link to="/new-meetups">New Meetups</Link>
+          <Link to="/favourites">
+            Favourites{" "}
+            <span className={classes.badge}>{favouritCtx.totalFavourites}</span>
+          </Link>
         </li>
       </ul>
     </div>
